08313620bb82baf260f360f3aedd4cf7da766017
Samarth Goyal <goyalsamarth4888@gmail.com>
2026-04-11 00:44:34 +0530

    Time: 1 ms (99.98%) | Memory: 46.7 MB (86.38%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-        
-        for(int num : nums) {
-            result ^= num;
+        int num;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            result = result ^ nums[i];
         }
-        
         return result;
     }
 }
